e275e7705ab39fac9af0005f58ba49876131ad33
Sergio Zamudio <szamu5@hotmail.com>
2019-09-25 16:47:45 -0500

    FIX: pchar div

--- op_operators.c
+++ op_operators.c
@@ -49,7 +49,7 @@ void op_div(stack_t **head, unsigned int line)
 {
 	if (*head == NULL || (**head).next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line);
 		exit(EXIT_FAILURE);
 	}
 	if ((**head).n == 0)
